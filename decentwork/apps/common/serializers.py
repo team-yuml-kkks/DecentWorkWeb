8e8867ff6b01865da2f930b82116a22c3359bced
kaniak274 <kamil.kucharski@makimo.pl>
2018-11-07 12:02:03 +0100

    Add api register.

---
+++ decentwork/apps/common/serializers.py
@@ -0,0 +1,25 @@
+from django.core.exceptions import ValidationError
+from rest_framework import serializers
+
+from decentwork.apps.common.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    def validate_email(self, value):
+        """Validate is unique."""
+        user = User.objects.filter(email=value)
+
+        if user:
+            raise ValidationError('Taki email już istnieje.')
+
+        return value
+
+    def save(self, password):
+        email = self.validated_data['email']
+
+        User.objects.create_user(username=email, email=email, password=password)
+
+    class Meta:
+        model = User
+        fields = ('email',)
--- decentwork/apps/common/urls.py
+++ decentwork/apps/common/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url
-from django.urls import path, re_path
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [] + router.urls
