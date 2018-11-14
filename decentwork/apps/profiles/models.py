1b1e9d4c77b3728f92a708d1898cc1c1fe60e31d
kaniak274 <kamil.kucharski@makimo.pl>
2018-11-14 13:40:30 +0100

    Add viewset, serializer, and router to urls for UserProfile model.

--- decentwork/apps/profiles/models.py
+++ decentwork/apps/profiles/models.py
@@ -22,7 +22,7 @@ class UserProfile(models.Model):
     professions = models.ManyToManyField(Profession, blank=True)
 
     description = models.TextField(blank=True, null=True)
-    phone_numbers = ArrayField(models.CharField(max_length=11, blank=True, null=True))
+    phone_numbers = ArrayField(models.CharField(max_length=11, blank=True, null=True), blank=True, null=True)
 
     def __str__(self):
-        return self.username
+        return self.user.email
