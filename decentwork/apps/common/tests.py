ea2d1ecddd9139794a6e45196ea121c56fa32e3f
kaniak <kamil.kucharski@makimo.pl>
2018-11-23 14:36:23 +0100

    Add login tests.

--- decentwork/apps/common/tests.py
+++ decentwork/apps/common/tests.py
@@ -77,3 +77,46 @@ class UserApiTests(APITestCase):
         data = {'email': 'test2@test.com'}
         response = self.apiclient.put('/common/users/2/', data)
         self.assertEqual(response.status_code, 404)
+
+    def test_login_when_everything_is_fine(self):
+        """Tests login when success and reponse is 200."""
+        data = {'email': 'test@test.com', 'password': 'test1234#'}
+        response = self.client.post('/common/login/', data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'email': 'test@test.com'})
+
+    def test_login_when_wrong_email_format(self):
+        """Tests login when email has wrong format."""
+        data = {'email': 'testtest.com', 'password': 'test1234#'}
+        response = self.client.post('/common/login/', data)
+        self.assertEqual(response.status_code, 400)
+
+    def test_login_when_no_email(self):
+        """Tests login when no email in POST data."""
+        data = {'password': 'test1234#'}
+        response = self.client.post('/common/login/', data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_login_when_no_password(self):
+        """Tests login when no password in POST data."""
+        data = {'email': 'test@test.com'}
+        response = self.client.post('/common/login/', data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_login_when_wrong_password(self):
+        """Tests login when wrong password is entered."""
+        data = {'email': 'test@test.com', 'password': 'test1234'}
+        response = self.client.post('/common/login/', data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_login_when_wrong_email(self):
+        """Tests login when wrong email is entered."""
+        data = {'email': 'tes@test.com', 'password': 'test1234#'}
+        response = self.client.post('/common/login/', data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_login_when_wrong_email_and_password(self):
+        """Tests login when wrong email and password are entered."""
+        data = {'email': 'tes@test.com', 'password': 'test1234'}
+        response = self.client.post('/common/login/', data)
+        self.assertEqual(response.status_code, 401)
