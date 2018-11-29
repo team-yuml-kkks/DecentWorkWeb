70811f75b249d0b60b8ce9aae13ea65e23f43caf
kaniak <kamil.kucharski@makimo.pl>
2018-11-29 16:29:58 +0100

    Add full handling for registering mobile user with GoogleAuth

--- decentwork/apps/common/views.py
+++ decentwork/apps/common/views.py
@@ -1,3 +1,7 @@
+from typing import Dict, Optional
+
+from allauth.socialaccount.models import SocialAccount
+from allauth.account.adapter import get_adapter
 from django.conf import settings
 from django.contrib.auth import authenticate
 from google.oauth2 import id_token
@@ -53,9 +57,17 @@ class UserApiLogin(APIView):
 
 
 class TokenSignIn(APIView):
+    """Handle user sign in from mobile device."""
     authentication_classes = ()
 
     def post(self, request, format=None) -> Response:
+        """Creates user with data from Google oauth2 token when email doesn't exist in database.
+        
+        Returns:
+            Response with UserLoginSerializer data and status code equal 200 if everything went okay.
+            Response with status code equal 400 when user didn't send idToken with request.
+            Response with status code equal 401 when token is invalid.
+        """
         token = request.data.get('idToken', None)
         
         if token:
@@ -64,7 +76,53 @@ class TokenSignIn(APIView):
             if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
                 return Response("Token jest zły", status=status.HTTP_401_UNAUTHORIZED)
 
-            userid = idinfo['sub']
-            print(userid)
+            email = idinfo['email']
+            user =  User.objects.filter(email=email).first()
+
+            if user is None:
+                new_user = self._create_user(idinfo)
+
+                social_user = SocialAccount.create(
+                    user=user,
+                    provider='google',
+                    uid=idinfo['sub'],
+                    extra_data=idinfo
+                )
+
+                serializer = self._serialize_user_data(new_user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                serializer = self._serialize_user_data(user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response("Brak tokenu", status=status.HTTP_400_BAD_REQUEST)
+
+    def _serialize_user_data(self, user: User) -> UserLoginSerializer:
+        """Serialize user data.
+        
+        Args:
+            user: User object to serialize.
+        """
+        return UserLoginSerializer(
+            id=user.id,
+            email=user.email,
+        )
+
+    def _create_user(self, idinfo: Dict) -> User:
+        """Creates new User.
+        
+        Args:
+            ididnfo: Information from Google about user.
+        
+        Returns:
+            Saved new user.
+        """
+        user = User()
+        user.set_unusable_password(True)
+        user.email = idinfo['email']
+        user.username = idinfo['email']
+        user.first_name = idinfo['given_name']
+        user.last_name = idinfo['family_name']
+        user.save()
+
+        return user
