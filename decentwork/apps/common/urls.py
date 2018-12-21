ebd1f19a0ba89116ed2adc8da65685b182136446
kaniak <kamil.kucharski@makimo.pl>
2018-12-21 09:31:59 +0100

    Remove useless code

--- decentwork/apps/common/urls.py
+++ decentwork/apps/common/urls.py
@@ -4,10 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
-
 urlpatterns = [
     path('login/', views.UserApiLogin.as_view()),
     path('tokensignin/', views.TokenSignIn.as_view()),
-] + router.urls
+]
