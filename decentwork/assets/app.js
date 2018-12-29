009a0607a56950b13200364b10bb6115ddab35a3
kaniak274 <kaniak274@gmail.com>
2018-12-29 21:14:54 +0100

    Update app.js

--- decentwork/assets/app.js
+++ decentwork/assets/app.js
@@ -1,14 +1,12 @@
 import 'bootstrap';
 
 import Vue from 'vue';
-import VueSession from 'vue-session'
 import App from './app.vue';
 import { store } from './store';
 import { router } from './router';
 import './scss/main.scss';
 
 Vue.use(require('vue-cookies'));
-Vue.use(VueSession);
 Vue.component('navbar', require('./components/navbar.vue').default);
 
 var app = new Vue({
