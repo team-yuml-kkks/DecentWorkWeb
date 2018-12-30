f0b4c914525db4f3385901dfbbc090fc62168948
Kamil Supera <kamil.supera@makimo.pl>
2018-12-30 02:46:24 +0100

    Change basic style, add to vue import awsome font

--- decentwork/assets/app.js
+++ decentwork/assets/app.js
@@ -2,12 +2,19 @@ import 'bootstrap';
 
 import Vue from 'vue';
 import App from './app.vue';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { store } from './store';
 import { router } from './router';
 import './scss/main.scss';
 
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
 Vue.use(require('vue-cookies'));
 Vue.component('navbar', require('./components/navbar.vue').default);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+library.add(faUser);
 
 var app = new Vue({
     el: '#app',
--- decentwork/assets/components/home.vue
+++ decentwork/assets/components/home.vue
@@ -1,5 +1,4 @@
 <template>
     <div>
-        Home
     </div>
 </template>
