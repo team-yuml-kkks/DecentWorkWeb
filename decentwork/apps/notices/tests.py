4453b93c12cf180b80abd183caa4a1afd88cc93f
kaniak <kaniak274@gmail.com>
2019-01-05 21:04:21 +0100

    Fix notices app tests

--- decentwork/apps/notices/tests.py
+++ decentwork/apps/notices/tests.py
@@ -16,38 +16,38 @@ class NoticesApiTests(APITestCase):
 
     def test_list_method_status_200(self):
         """Test status 200 when getting /engagments."""
-        response = self.client.get('/engagments/engagments/')
+        response = self.client.get('/notices/notices/')
         self.assertEqual(response.status_code, 200)
 
     def test_list_method_response(self):
         """Test response data when getting list of engagments."""
-        response = self.client.get('/engagments/engagments/')
+        response = self.client.get('/notices/notices/')
         self.assertEqual(response.data['results'][0]['title'], 'Test')
         self.assertEqual(response.data['results'][1]['title'], 'Hello')
 
     def test_list_method_content_type(self):
         """Test content-type in viewset method."""
-        response = self.client.get('/engagments/engagments/')
+        response = self.client.get('/notices/notices/')
         self.assertEqual(response['Content-Type'], 'application/json')
 
     def test_retrieve_method_status_200(self):
         """Test response status from retrieve viewset method."""
-        response = self.client.get('/engagments/engagments/1/')
+        response = self.client.get('/notices/notices/1/')
         self.assertEqual(response.status_code, 200)
 
     def test_retrieve_method_contents_json(self):
         """Test response content-type in retrieve method."""
-        response = self.client.get('/engagments/engagments/1/')
+        response = self.client.get('/notices/notices/1/')
         self.assertEqual(response['Content-Type'], 'application/json')
 
     def test_retrive_response_json(self):
         """Test response data in retrieve method."""
-        response = self.client.get('/engagments/engagments/1/')
+        response = self.client.get('/notices/notices/1/')
         self.assertEqual(response.data['title'], 'Test')
 
     def test_create_without_token(self):
         """Test if create returns 401 when no token."""
-        response = self.client.post('/engagments/engagments/')
+        response = self.client.post('/notices/notices/')
         self.assertEqual(response.status_code, 401)
 
     def test_create_engagment_when_all_data_pass(self):
@@ -55,7 +55,7 @@ class NoticesApiTests(APITestCase):
         self._get_credentials()
         data = {'title': 'Hello2', 'description': 'lol',
                 'city': 'Warszawa', 'profession': 'Hydraulik'}
-        response = self.client.post('/engagments/engagments/', data)
+        response = self.client.post('/notices/notices/', data)
         self.assertEqual(response.status_code, 201)
 
     def test_create_engagment_when_no_city(self):
@@ -63,28 +63,28 @@ class NoticesApiTests(APITestCase):
         self._get_credentials()
         data = {'title': 'Hello2', 'description': 'lol',
                 'profession': 'Hydraulik'}
-        response = self.client.post('/engagments/engagments/', data)
+        response = self.client.post('/notices/notices/', data)
         self.assertEqual(response.status_code, 400)
 
     def test_create_engagment_when_no_profession(self):
         """Test create engagment returns 201 when no profession is passed."""
         self._get_credentials()
         data = {'title': 'Hello2', 'description': 'lol', 'city': 'Warszawa'}
-        response = self.client.post('/engagments/engagments/', data)
+        response = self.client.post('/notices/notices/', data)
         self.assertEqual(response.status_code, 201)
 
     def test_create_engagment_when_no_description(self):
         """Test create engagment returns 201 when no description is passed."""
         self._get_credentials()
         data = {'title': 'Hello2', 'city': 'Warszawa', 'profession': 'Hydraulik'}
-        response = self.client.post('/engagments/engagments/', data)
+        response = self.client.post('/notices/notices/', data)
         self.assertEqual(response.status_code, 201)
 
     def test_create_engagment_when_no_title(self):
         """Test create engagment returns 400 when no title passed."""
         self._get_credentials()
         data = {'city': 'Warszawa', 'profession': 'Hydraulik'}
-        response = self.client.post('/engagments/engagments/', data)
+        response = self.client.post('/notices/notices/', data)
         self.assertEqual(response.status_code, 400)
 
     def test_update_engagment(self):
@@ -92,53 +92,53 @@ class NoticesApiTests(APITestCase):
         self._get_credentials()
         data = {'title': 'Hello2', 'description': 'lol',
                 'city': 'Warszawa', 'profession': 'Hydraulik'}
-        response = self.client.put('/engagments/engagments/1/', data)
+        response = self.client.put('/notices/notices/1/', data)
         self.assertEqual(response.status_code, 200)
 
     def test_assign_engagment_and_user(self):
         """Test assigning engagment with user returns 201."""
         self._get_credentials()
-        data = {'engagment': 1}
-        response = self.client.post('/engagments/assign/user/', data)
+        data = {'notice': 1}
+        response = self.client.post('/notices/assign/user/', data)
         self.assertEqual(response.status_code, 201)
 
     def test_delete_assign(self):
         """Test if deleting assigment between engagment and user returns 204."""
         self._get_credentials()
-        response = self.client.delete('/engagments/assign/user/2/')
+        response = self.client.delete('/notices/assign/user/2/')
         self.assertEqual(response.status_code, 204)
 
     def test_assign_list(self):
         """Test showing list of users assigned to single engagment."""
-        data = {'engagment': 2}
-        response = self.client.get('/engagments/assign/list/', data)
+        data = {'notice': 2}
+        response = self.client.get('/notices/assign/list/', data)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data[0]['engagment'], 2)
+        self.assertEqual(response.data[0]['notice'], 2)
         self.assertEqual(response.data[0]['user'], 1)
-        self.assertEqual(response.data[1]['engagment'], 2)
+        self.assertEqual(response.data[1]['notice'], 2)
         self.assertEqual(response.data[1]['user'], 3)
 
     def test_assign_list_when_no_engagment_passed(self):
         """Test if assign list returns 400 when no engagment passed."""
-        response = self.client.get('/engagments/assign/list/')
+        response = self.client.get('/notices/assign/list/')
         self.assertEqual(response.status_code, 400)
 
     def test_assign_check_no_engagment(self):
         """Test if assign check returns 400 when no engagment passed."""
         self._get_credentials()
-        response = self.client.get('/engagments/assign/check/')
+        response = self.client.get('/notices/assign/check/')
         self.assertEqual(response.status_code, 400)
 
     def test_assign_check_no_credentials(self):
         """Test if assign check returns 401 when no credentials passed."""
-        response = self.client.get('/engagments/assign/check/')
+        response = self.client.get('/notices/assign/check/')
         self.assertEqual(response.status_code, 401)
 
     def test_assign_check(self):
         """Test assign_check when success."""
-        data = {'engagment': 1}
+        data = {'notice': 1}
         self._get_credentials()
-        response = self.client.get('/engagments/assign/check/', data)
+        response = self.client.get('/notices/assign/check/', data)
         self.assertEqual(response.status_code, 200)
 
     def test_assign_check_when_not_assigned(self):
@@ -146,9 +146,9 @@ class NoticesApiTests(APITestCase):
         Test if response contains key is_assigned and it contains False
         when user is not assigned to engagment.
         """
-        data = {'engagment': 1}
+        data = {'notice': 1}
         self._get_credentials()
-        response = self.client.get('/engagments/assign/check/', data)
+        response = self.client.get('/notices/assign/check/', data)
         self.assertEqual(response.data['is_assigned'], False)
 
     def test_assign_check_when_assigned(self):
@@ -156,7 +156,7 @@ class NoticesApiTests(APITestCase):
         Test if response contains key is_assigned and it contains True
         when user is assigned to engagment.
         """
-        data = {'engagment': 2}
+        data = {'notice': 2}
         self._get_credentials()
-        response = self.client.get('/engagments/assign/check/', data)
+        response = self.client.get('/notices/assign/check/', data)
         self.assertEqual(response.data['is_assigned'], True)
